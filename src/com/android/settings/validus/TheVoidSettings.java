f6b14f96e6fde490ecec9455f650d5df44c5dcf1
martinusbe <martinusbe@gmail.com>
2015-11-28 16:27:10 +0100

    Metricslogger for The Void
    Change-Id: I9c8d98ab5844b7886ddfb8e73ee2b50a2055c920
    Signed-off-by: martinusbe <martinusbe@gmail.com>

--- src/com/android/settings/validus/TheVoidSettings.java
+++ src/com/android/settings/validus/TheVoidSettings.java
@@ -3,10 +3,17 @@ package com.android.settings.validus;
 
 import android.os.Bundle;
 
+import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+
 public class SystemSettings extends SettingsPreferenceFragment {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.MAIN_SETTINGS;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
