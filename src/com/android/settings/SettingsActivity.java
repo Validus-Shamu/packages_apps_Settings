18c34e06437444c025b980676634a47cea9b50cc
Randall Rushing <randall.rushing@gmail.com>
2016-01-17 12:36:05 -0800

    SmartBar: expose new supporting SmartBar activities
    Change-Id: I6c76b6cf8b2bac218f9c586adeaa7be615b01a4e
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -72,6 +72,7 @@ import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.applications.DrawOverlayDetails;
 import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
+import com.android.settings.solo.NavbarSettings;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
@@ -362,6 +363,7 @@ public class SettingsActivity extends Activity
             WolvesDenSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             BlacklistSettings.class.getName()
+            NavbarSettings.class.getName()
     };
 
 
