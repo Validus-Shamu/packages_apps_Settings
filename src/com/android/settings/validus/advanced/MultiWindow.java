1c40915eef67705230d7e3dec6e31c1a1f7bb460
theringmaster <gzroms@hotmail.com>
2016-03-26 12:28:00 -0400

    Validus

--- src/com/android/settings/validus/advanced/MultiWindow.java
+++ src/com/android/settings/validus/advanced/MultiWindow.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.aosip.advanced;
+package com.android.settings.validus.advanced;
 
 import android.content.Context;
 import android.content.ContentResolver;
