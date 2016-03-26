xlxfoxxlx/platform_packages_apps_Settings-2

Add quicksettings category and add back icon settings in statusbar

src/com/android/settings/aosip/qssettings/ExpandedCategory.java
@@ -1,0 +1,59 @@
+/*
+ * Copyright (C) 2015 Android Open Source Illusion Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aosip.qssettings;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class ExpandedCategory extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.OWLSNEST;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.aosip_expanded);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
+

src/com/android/settings/aosip/qssettings/QsSettingsHolder.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.aosip.statusbar;
+package com.android.settings.aosip.qssettings;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -51,17 +51,13 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
 
-import com.android.settings.aosip.statusbar.ClockCategory;
-import com.android.settings.aosip.statusbar.CarrierlabelCategory;
-import com.android.settings.aosip.statusbar.BatteryCategory;
-import com.android.settings.aosip.statusbar.IconsCategory;
-import com.android.settings.aosip.statusbar.ExtrasCategory;
+import com.android.settings.aosip.qssettings.ExpandedCategory;
 import com.android.settings.aosip.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class StatusbarHolder extends SettingsPreferenceFragment {
+public class QsSettingsHolder extends SettingsPreferenceFragment {
 
     ViewPager mViewPager;
     String titleString[];
@@ -110,12 +106,8 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
 
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
-            frags[0] = new ClockCategory();
-            frags[1] = new BatteryCategory();
-            frags[2] = new IconsCategory();
-            frags[3] = new CarrierlabelCategory();
-            frags[4] = new TrafficCategory();
-            //frags[4] = new ExtrasCategory();
+            frags[0] = new ExpandedCategory();
+            //frags[1] = new BatteryCategory();
         }
 
         @Override
@@ -137,12 +129,8 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-                    getString(R.string.clock_category),
-                    getString(R.string.battery_category),
-                    getString(R.string.icon_category),
-                    getString(R.string.carrier_category),
-                    getString(R.string.traffic_category)};
-                    //getString(R.string.extra_category)};
+                    //getString(R.string.clock_category),
+                    getString(R.string.expanded_category)};
         return titleString;
     }
 }

Validus-Shamu/packages_apps_Settings

src/com/android/settings/validus/qssettings/ExpandedCategory.java
@@ -1,0 +1,59 @@
+/*
+ * Copyright (C) 2015 Android Open Source Illusion Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.validus.qssettings;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class ExpandedCategory extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.THE_WOLF;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.validus_expanded);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
+

src/com/android/settings/validus/qssettings/QsSettingsHolder.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.validus.statusbar;
+package com.android.settings.validus.qssettings;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -51,17 +51,13 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
 
-import com.android.settings.validus.statusbar.ClockCategory;
-import com.android.settings.validus.statusbar.CarrierlabelCategory;
-import com.android.settings.validus.statusbar.BatteryCategory;
-import com.android.settings.validus.statusbar.IconsCategory;
-import com.android.settings.validus.statusbar.ExtrasCategory;
+import com.android.settings.validus.qssettings.ExpandedCategory;
 import com.android.settings.validus.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class StatusbarHolder extends SettingsPreferenceFragment {
+public class QsSettingsHolder extends SettingsPreferenceFragment {
 
     ViewPager mViewPager;
     String titleString[];
@@ -110,12 +106,8 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
 
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
-            frags[0] = new ClockCategory();
-            frags[1] = new BatteryCategory();
-            frags[2] = new IconsCategory();
-            frags[3] = new CarrierlabelCategory();
-            frags[4] = new TrafficCategory();
-            //frags[4] = new ExtrasCategory();
+            frags[0] = new ExpandedCategory();
+            //frags[1] = new BatteryCategory();
         }
 
         @Override
@@ -137,12 +129,8 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-                    getString(R.string.clock_category),
-                    getString(R.string.battery_category),
-                    getString(R.string.icon_category),
-                    getString(R.string.carrier_category),
-                    getString(R.string.traffic_category)};
-                    //getString(R.string.extra_category)};
+                    //getString(R.string.clock_category),
+                    getString(R.string.expanded_category)};
         return titleString;
     }
 }