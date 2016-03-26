xlxfoxxlx/platform_packages_apps_Settings-2

Lockscreen weather this is a configuration from several different sources worked into one

src/com/android/settings/aosip/lockscreen/WeatherCategory.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2015 Android Open Source Illusion Project
+/* 
+ * Copyright (C) 2014 DarkKat
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,23 +16,69 @@
 
 package com.android.settings.aosip.lockscreen;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.res.Resources;
+import android.content.ContentResolver;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.internal.logging.MetricsLogger;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.aosip.seekbar.SeekBarPreference;
 
 public class WeatherCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_CONDITION_ICON =
+            "weather_condition_icon";
+    private static final String PREF_HIDE_WEATHER =
+            "weather_hide_panel";
+    private static final String PREF_NUMBER_OF_NOTIFICATIONS =
+            "weather_number_of_notifications";
+
+    private static final int MONOCHROME_ICON = 0;
+
+    private ListPreference mConditionIcon;
+    private ListPreference mHideWeather;
+    private SeekBarPreference mNumberOfNotifications;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.aosip_weather);
+        mResolver = getActivity().getContentResolver();
+        PreferenceScreen prefs = getPreferenceScreen();
+
+        mConditionIcon =
+                (ListPreference) findPreference(PREF_CONDITION_ICON);
+        int conditionIcon = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
+        mConditionIcon.setValue(String.valueOf(conditionIcon));
+        mConditionIcon.setSummary(mConditionIcon.getEntry());
+        mConditionIcon.setOnPreferenceChangeListener(this);
+
+        mHideWeather =
+                (ListPreference) findPreference(PREF_HIDE_WEATHER);
+        int hideWeather = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, 0);
+        mHideWeather.setValue(String.valueOf(hideWeather));
+        mHideWeather.setOnPreferenceChangeListener(this);
+
+        mNumberOfNotifications =
+                (SeekBarPreference) findPreference(PREF_NUMBER_OF_NOTIFICATIONS);
+        int numberOfNotifications = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS, 4);
+        mNumberOfNotifications.setValue(numberOfNotifications);
+        mNumberOfNotifications.setOnPreferenceChangeListener(this);
+
+        updatePreference();
+    }
 
     @Override
     protected int getMetricsCategory() {
@@ -40,20 +86,51 @@ public class WeatherCategory extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.aosip_weather);
-
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
+        updatePreference();
+    }
+
+    private void updatePreference() {
+        int hideWeather = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, 0);
+        if (hideWeather == 0) {
+            mNumberOfNotifications.setEnabled(false);
+            mHideWeather.setSummary(R.string.weather_hide_panel_auto_summary);
+        } else if (hideWeather == 1) {
+            mNumberOfNotifications.setEnabled(true);
+            mHideWeather.setSummary(R.string.weather_hide_panel_custom_summary);
+        } else {
+            mNumberOfNotifications.setEnabled(false);
+            mHideWeather.setSummary(R.string.weather_hide_panel_never_summary);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        if (preference == mConditionIcon) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mConditionIcon.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
+            mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
+            return true;
+        } else if (preference == mHideWeather) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mHideWeather.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, intValue);
+            updatePreference();
+            return true;
+        } else if (preference == mNumberOfNotifications) {
+            int numberOfNotifications = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS,
+            numberOfNotifications);
+            return true;
+        }
         return false;
     }
 }
+
 

src/com/android/settings/aosip/seekbar/SeekBarPreference.java
@@ -1,0 +1,229 @@
+package com.android.settings.aosip.seekbar;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import com.android.settings.R;
+
+public class SeekBarPreference extends Preference implements OnSeekBarChangeListener {
+
+    private final String TAG = getClass().getName();
+
+    private static final String ANDROIDNS = "http://schemas.android.com/apk/res/android";
+    private static final String SETTINGS = "http://schemas.android.com/apk/res/com.android.settings";
+    private static final int DEFAULT_VALUE = 50;
+
+    private int mMaxValue      = 100;
+    private int mMinValue      = 0;
+    private int mInterval      = 1;
+    private int mCurrentValue;
+    private String mUnitsLeft  = "";
+    private String mUnitsRight = "";
+    private SeekBar mSeekBar;
+    private TextView mTitle;
+
+    private TextView mStatusText;
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initPreference(context, attrs);
+    }
+
+    public SeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initPreference(context, attrs);
+    }
+
+    private void initPreference(Context context, AttributeSet attrs) {
+        setValuesFromXml(attrs);
+        mSeekBar = new SeekBar(context, attrs);
+        mSeekBar.setMax(mMaxValue - mMinValue);
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    private void setValuesFromXml(AttributeSet attrs) {
+        final TypedArray typedArray = getContext().obtainStyledAttributes(
+                attrs, R.styleable.SeekBarPreference);
+
+        mMaxValue = attrs.getAttributeIntValue(ANDROIDNS, "max", 100);
+        mMinValue = attrs.getAttributeIntValue(SETTINGS, "minimum", 0);
+        mUnitsLeft = getAttributeStringValue(attrs, SETTINGS, "unitsLeft", "");
+        String units = getAttributeStringValue(attrs, SETTINGS, "units", "");
+        mUnitsRight = getAttributeStringValue(attrs, SETTINGS, "unitsRight", units);
+
+        Integer id = typedArray.getResourceId(R.styleable.SeekBarPreference_unitsRight, 0);
+        if (id > 0) {
+            mUnitsRight = getContext().getResources().getString(id);
+        }
+        id = typedArray.getResourceId(R.styleable.SeekBarPreference_unitsLeft, 0);
+        if (id > 0) {
+            mUnitsLeft = getContext().getResources().getString(id);
+        }
+
+        try {
+            String newInterval = attrs.getAttributeValue(SETTINGS, "interval");
+            if(newInterval != null)
+                mInterval = Integer.parseInt(newInterval);
+        }
+        catch(Exception e) {
+            Log.e(TAG, "Invalid interval value", e);
+        }
+    }
+
+    private String getAttributeStringValue(AttributeSet attrs, String namespace, String name, String defaultValue) {
+        String value = attrs.getAttributeValue(namespace, name);
+        if(value == null)
+            value = defaultValue;
+
+        return value;
+    }
+
+    @Override
+    public void onDependencyChanged(Preference dependency, boolean disableDependent) {
+        super.onDependencyChanged(dependency, disableDependent);
+        this.setShouldDisableView(true);
+        if (mTitle != null)
+            mTitle.setEnabled(!disableDependent);
+        if (mSeekBar != null)
+            mSeekBar.setEnabled(!disableDependent);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent){
+
+        RelativeLayout layout =  null;
+        try {
+            LayoutInflater mInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            layout = (RelativeLayout)mInflater.inflate(R.layout.seek_bar_preference, parent, false);
+            mTitle = (TextView) layout.findViewById(android.R.id.title);
+        }
+        catch(Exception e)
+        {
+            Log.e(TAG, "Error creating seek bar preference", e);
+        }
+        return layout;
+    }
+
+    @Override
+    public void onBindView(View view) {
+        super.onBindView(view);
+        try
+        {
+            // move our seekbar to the new view we've been given
+            ViewParent oldContainer = mSeekBar.getParent();
+            ViewGroup newContainer = (ViewGroup) view.findViewById(R.id.seekBarPrefBarContainer);
+
+            if (oldContainer != newContainer) {
+                // remove the seekbar from the old view
+                if (oldContainer != null) {
+                    ((ViewGroup) oldContainer).removeView(mSeekBar);
+                }
+                // remove the existing seekbar (there may not be one) and add ours
+                newContainer.removeAllViews();
+                newContainer.addView(mSeekBar, ViewGroup.LayoutParams.FILL_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+            }
+        }
+        catch(Exception ex) {
+            Log.e(TAG, "Error binding view: " + ex.toString());
+        }
+        updateView(view);
+    }
+
+    /**
+     * Update a SeekBarPreference view with our current state
+     * @param view
+     */
+    protected void updateView(View view) {
+
+        try {
+            RelativeLayout layout = (RelativeLayout)view;
+            mStatusText = (TextView)layout.findViewById(R.id.seekBarPrefValue);
+            mStatusText.setText(String.valueOf(mCurrentValue));
+            mStatusText.setMinimumWidth(30);
+            mSeekBar.setProgress(mCurrentValue - mMinValue);
+
+            TextView unitsRight = (TextView)layout.findViewById(R.id.seekBarPrefUnitsRight);
+            unitsRight.setText(mUnitsRight);
+            TextView unitsLeft = (TextView)layout.findViewById(R.id.seekBarPrefUnitsLeft);
+            unitsLeft.setText(mUnitsLeft);
+        }
+        catch(Exception e) {
+            Log.e(TAG, "Error updating seek bar preference", e);
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int newValue = progress + mMinValue;
+        if(newValue > mMaxValue)
+            newValue = mMaxValue;
+        else if(newValue < mMinValue)
+            newValue = mMinValue;
+        else if(mInterval != 1 && newValue % mInterval != 0)
+            newValue = Math.round(((float)newValue)/mInterval)*mInterval;
+
+        // change rejected, revert to the previous value
+        if(!callChangeListener(newValue)){
+            seekBar.setProgress(mCurrentValue - mMinValue);
+            return;
+        }
+        // change accepted, store it
+        mCurrentValue = newValue;
+        mStatusText.setText(String.valueOf(newValue));
+        persistInt(newValue);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        notifyChanged();
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray ta, int index){
+        int defaultValue = ta.getInt(index, DEFAULT_VALUE);
+        return defaultValue;
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        if(restoreValue) {
+            mCurrentValue = getPersistedInt(mCurrentValue);
+        }
+        else {
+            int temp = 0;
+            try {
+                temp = (Integer)defaultValue;
+            }
+            catch(Exception ex) {
+                Log.e(TAG, "Invalid default value: " + defaultValue.toString());
+            }
+            persistInt(temp);
+            mCurrentValue = temp;
+        }
+    }
+
+    public void setValue(int value) {
+        mCurrentValue = value;
+    }
+
+    @Override
+    public void setEnabled (boolean enabled) {
+        mSeekBar.setEnabled(enabled);
+        super.setEnabled(enabled);
+    }
+}
+

Validus-Shamu/packages_apps_Settings

src/com/android/settings/validus/lockscreen/WeatherCategory.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2015 Android Open Source Illusion Project
+/* 
+ * Copyright (C) 2014 DarkKat
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,44 +16,119 @@
 
 package com.android.settings.validus.lockscreen;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.res.Resources;
+import android.content.ContentResolver;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.internal.logging.MetricsLogger;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.validus.seekbar.SeekBarPreference;
 
 public class WeatherCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    @Override
-    protected int getMetricsCategory() {
-        return MetricsLogger.THE_WOLF;
-    }
+    private static final String PREF_CONDITION_ICON =
+            "weather_condition_icon";
+    private static final String PREF_HIDE_WEATHER =
+            "weather_hide_panel";
+    private static final String PREF_NUMBER_OF_NOTIFICATIONS =
+            "weather_number_of_notifications";
+
+    private static final int MONOCHROME_ICON = 0;
+
+    private ListPreference mConditionIcon;
+    private ListPreference mHideWeather;
+    private SeekBarPreference mNumberOfNotifications;
+
+    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.gz_weather);
+        mResolver = getActivity().getContentResolver();
+        PreferenceScreen prefs = getPreferenceScreen();
 
-        addPreferencesFromResource(R.xml.validus_weather);
+        mConditionIcon =
+                (ListPreference) findPreference(PREF_CONDITION_ICON);
+        int conditionIcon = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
+        mConditionIcon.setValue(String.valueOf(conditionIcon));
+        mConditionIcon.setSummary(mConditionIcon.getEntry());
+        mConditionIcon.setOnPreferenceChangeListener(this);
 
+        mHideWeather =
+                (ListPreference) findPreference(PREF_HIDE_WEATHER);
+        int hideWeather = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, 0);
+        mHideWeather.setValue(String.valueOf(hideWeather));
+        mHideWeather.setOnPreferenceChangeListener(this);
+
+        mNumberOfNotifications =
+                (SeekBarPreference) findPreference(PREF_NUMBER_OF_NOTIFICATIONS);
+        int numberOfNotifications = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS, 4);
+        mNumberOfNotifications.setValue(numberOfNotifications);
+        mNumberOfNotifications.setOnPreferenceChangeListener(this);
+
+        updatePreference();
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.OWLSNEST;
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        updatePreference();
+    }
+
+    private void updatePreference() {
+        int hideWeather = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, 0);
+        if (hideWeather == 0) {
+            mNumberOfNotifications.setEnabled(false);
+            mHideWeather.setSummary(R.string.weather_hide_panel_auto_summary);
+        } else if (hideWeather == 1) {
+            mNumberOfNotifications.setEnabled(true);
+            mHideWeather.setSummary(R.string.weather_hide_panel_custom_summary);
+        } else {
+            mNumberOfNotifications.setEnabled(false);
+            mHideWeather.setSummary(R.string.weather_hide_panel_never_summary);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        if (preference == mConditionIcon) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mConditionIcon.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
+            mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
+            return true;
+        } else if (preference == mHideWeather) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mHideWeather.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, intValue);
+            updatePreference();
+            return true;
+        } else if (preference == mNumberOfNotifications) {
+            int numberOfNotifications = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS,
+            numberOfNotifications);
+            return true;
+        }
         return false;
     }
 }
-

src/com/android/settings/validus/seekbar/SeekBarPreference.java
@@ -1,0 +1,229 @@
+package com.android.settings.validus.seekbar;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import com.android.settings.R;
+
+public class SeekBarPreference extends Preference implements OnSeekBarChangeListener {
+
+    private final String TAG = getClass().getName();
+
+    private static final String ANDROIDNS = "http://schemas.android.com/apk/res/android";
+    private static final String SETTINGS = "http://schemas.android.com/apk/res/com.android.settings";
+    private static final int DEFAULT_VALUE = 50;
+
+    private int mMaxValue      = 100;
+    private int mMinValue      = 0;
+    private int mInterval      = 1;
+    private int mCurrentValue;
+    private String mUnitsLeft  = "";
+    private String mUnitsRight = "";
+    private SeekBar mSeekBar;
+    private TextView mTitle;
+
+    private TextView mStatusText;
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initPreference(context, attrs);
+    }
+
+    public SeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initPreference(context, attrs);
+    }
+
+    private void initPreference(Context context, AttributeSet attrs) {
+        setValuesFromXml(attrs);
+        mSeekBar = new SeekBar(context, attrs);
+        mSeekBar.setMax(mMaxValue - mMinValue);
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    private void setValuesFromXml(AttributeSet attrs) {
+        final TypedArray typedArray = getContext().obtainStyledAttributes(
+                attrs, R.styleable.SeekBarPreference);
+
+        mMaxValue = attrs.getAttributeIntValue(ANDROIDNS, "max", 100);
+        mMinValue = attrs.getAttributeIntValue(SETTINGS, "minimum", 0);
+        mUnitsLeft = getAttributeStringValue(attrs, SETTINGS, "unitsLeft", "");
+        String units = getAttributeStringValue(attrs, SETTINGS, "units", "");
+        mUnitsRight = getAttributeStringValue(attrs, SETTINGS, "unitsRight", units);
+
+        Integer id = typedArray.getResourceId(R.styleable.SeekBarPreference_unitsRight, 0);
+        if (id > 0) {
+            mUnitsRight = getContext().getResources().getString(id);
+        }
+        id = typedArray.getResourceId(R.styleable.SeekBarPreference_unitsLeft, 0);
+        if (id > 0) {
+            mUnitsLeft = getContext().getResources().getString(id);
+        }
+
+        try {
+            String newInterval = attrs.getAttributeValue(SETTINGS, "interval");
+            if(newInterval != null)
+                mInterval = Integer.parseInt(newInterval);
+        }
+        catch(Exception e) {
+            Log.e(TAG, "Invalid interval value", e);
+        }
+    }
+
+    private String getAttributeStringValue(AttributeSet attrs, String namespace, String name, String defaultValue) {
+        String value = attrs.getAttributeValue(namespace, name);
+        if(value == null)
+            value = defaultValue;
+
+        return value;
+    }
+
+    @Override
+    public void onDependencyChanged(Preference dependency, boolean disableDependent) {
+        super.onDependencyChanged(dependency, disableDependent);
+        this.setShouldDisableView(true);
+        if (mTitle != null)
+            mTitle.setEnabled(!disableDependent);
+        if (mSeekBar != null)
+            mSeekBar.setEnabled(!disableDependent);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent){
+
+        RelativeLayout layout =  null;
+        try {
+            LayoutInflater mInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            layout = (RelativeLayout)mInflater.inflate(R.layout.seek_bar_preference, parent, false);
+            mTitle = (TextView) layout.findViewById(android.R.id.title);
+        }
+        catch(Exception e)
+        {
+            Log.e(TAG, "Error creating seek bar preference", e);
+        }
+        return layout;
+    }
+
+    @Override
+    public void onBindView(View view) {
+        super.onBindView(view);
+        try
+        {
+            // move our seekbar to the new view we've been given
+            ViewParent oldContainer = mSeekBar.getParent();
+            ViewGroup newContainer = (ViewGroup) view.findViewById(R.id.seekBarPrefBarContainer);
+
+            if (oldContainer != newContainer) {
+                // remove the seekbar from the old view
+                if (oldContainer != null) {
+                    ((ViewGroup) oldContainer).removeView(mSeekBar);
+                }
+                // remove the existing seekbar (there may not be one) and add ours
+                newContainer.removeAllViews();
+                newContainer.addView(mSeekBar, ViewGroup.LayoutParams.FILL_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+            }
+        }
+        catch(Exception ex) {
+            Log.e(TAG, "Error binding view: " + ex.toString());
+        }
+        updateView(view);
+    }
+
+    /**
+     * Update a SeekBarPreference view with our current state
+     * @param view
+     */
+    protected void updateView(View view) {
+
+        try {
+            RelativeLayout layout = (RelativeLayout)view;
+            mStatusText = (TextView)layout.findViewById(R.id.seekBarPrefValue);
+            mStatusText.setText(String.valueOf(mCurrentValue));
+            mStatusText.setMinimumWidth(30);
+            mSeekBar.setProgress(mCurrentValue - mMinValue);
+
+            TextView unitsRight = (TextView)layout.findViewById(R.id.seekBarPrefUnitsRight);
+            unitsRight.setText(mUnitsRight);
+            TextView unitsLeft = (TextView)layout.findViewById(R.id.seekBarPrefUnitsLeft);
+            unitsLeft.setText(mUnitsLeft);
+        }
+        catch(Exception e) {
+            Log.e(TAG, "Error updating seek bar preference", e);
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int newValue = progress + mMinValue;
+        if(newValue > mMaxValue)
+            newValue = mMaxValue;
+        else if(newValue < mMinValue)
+            newValue = mMinValue;
+        else if(mInterval != 1 && newValue % mInterval != 0)
+            newValue = Math.round(((float)newValue)/mInterval)*mInterval;
+
+        // change rejected, revert to the previous value
+        if(!callChangeListener(newValue)){
+            seekBar.setProgress(mCurrentValue - mMinValue);
+            return;
+        }
+        // change accepted, store it
+        mCurrentValue = newValue;
+        mStatusText.setText(String.valueOf(newValue));
+        persistInt(newValue);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        notifyChanged();
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray ta, int index){
+        int defaultValue = ta.getInt(index, DEFAULT_VALUE);
+        return defaultValue;
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        if(restoreValue) {
+            mCurrentValue = getPersistedInt(mCurrentValue);
+        }
+        else {
+            int temp = 0;
+            try {
+                temp = (Integer)defaultValue;
+            }
+            catch(Exception ex) {
+                Log.e(TAG, "Invalid default value: " + defaultValue.toString());
+            }
+            persistInt(temp);
+            mCurrentValue = temp;
+        }
+    }
+
+    public void setValue(int value) {
+        mCurrentValue = value;
+    }
+
+    @Override
+    public void setEnabled (boolean enabled) {
+        mSeekBar.setEnabled(enabled);
+        super.setEnabled(enabled);
+    }
+}
+