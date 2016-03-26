xlxfoxxlx/platform_packages_apps_Settings-2

QS header transparency, Power/reboot menu transparency
Credit aicp: https://github.com/AICP/packages_apps_AicpExtras/commit/d369ead520f546665081431a8eff19ec9db23af9
https://github.com/AICP/packages_apps_AicpExtras/commit/05e5f262bb64a03f49225db42dc41a13cc42f1e3

Also moved shade transparency to tile screen

src/com/android/settings/aosip/qssettings/TileCategory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Android Open Source Illusion Project
+ * Copyright (C) 2015 Androis Open Source Illusion Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,23 +16,64 @@
 
 package com.android.settings.aosip.qssettings;
 
-import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.PreferenceCategory;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import com.android.internal.logging.MetricsLogger;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.aosip.seekbar.SeekBarPreference;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.Utils;
 
 public class TileCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+    
+    private static final String PREF_QS_TRANSPARENT_SHADE = "qs_transparent_shade";
+
+    private SeekBarPreference mQSShadeAlpha;
+
+    private final Configuration mCurConfig = new Configuration();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.aosip_tiles);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        // QS shade alpha
+        mQSShadeAlpha =
+                (SeekBarPreference) prefSet.findPreference(PREF_QS_TRANSPARENT_SHADE);
+        int qSShadeAlpha = Settings.System.getInt(resolver,
+                Settings.System.QS_TRANSPARENT_SHADE, 255);
+        mQSShadeAlpha.setValue(qSShadeAlpha / 1);
+        mQSShadeAlpha.setOnPreferenceChangeListener(this);
+    }
 
     @Override
     protected int getMetricsCategory() {
@@ -40,20 +81,30 @@ public class TileCategory extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.aosip_tiles);
-
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+        final String key = preference.getKey();
+        if (preference == mQSShadeAlpha) {
+                int alpha = (Integer) newValue;
+                Settings.System.putInt(getActivity().getContentResolver(),
+                        Settings.System.QS_TRANSPARENT_SHADE, alpha * 1);
+                return true;
+        }
+         return false;
     }
 }
+
 

Validus-Shamu/packages_apps_Settings

src/com/android/settings/validus/qssettings/TileCategory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Android Open Source Illusion Project
+ * Copyright (C) 2015 Androis Open Source Illusion Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,23 +16,64 @@
 
 package com.android.settings.validus.qssettings;
 
-import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.PreferenceCategory;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import com.android.internal.logging.MetricsLogger;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.validus.seekbar.SeekBarPreference;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.Utils;
 
 public class TileCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+    
+    private static final String PREF_QS_TRANSPARENT_SHADE = "qs_transparent_shade";
+
+    private SeekBarPreference mQSShadeAlpha;
+
+    private final Configuration mCurConfig = new Configuration();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.validus_tiles);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        // QS shade alpha
+        mQSShadeAlpha =
+                (SeekBarPreference) prefSet.findPreference(PREF_QS_TRANSPARENT_SHADE);
+        int qSShadeAlpha = Settings.System.getInt(resolver,
+                Settings.System.QS_TRANSPARENT_SHADE, 255);
+        mQSShadeAlpha.setValue(qSShadeAlpha / 1);
+        mQSShadeAlpha.setOnPreferenceChangeListener(this);
+    }
 
     @Override
     protected int getMetricsCategory() {
@@ -40,20 +81,28 @@ public class TileCategory extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.validus_tiles);
-
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+        final String key = preference.getKey();
+        if (preference == mQSShadeAlpha) {
+                int alpha = (Integer) newValue;
+                Settings.System.putInt(getActivity().getContentResolver(),
+                        Settings.System.QS_TRANSPARENT_SHADE, alpha * 1);
+                return true;
+        }
+         return false;
     }
 }
-