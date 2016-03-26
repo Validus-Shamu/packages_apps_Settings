xlxfoxxlx/platform_packages_apps_Settings-2

Remove some ras

src/com/android/settings/aosip/buttons/VolumeCategory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Android Open Source Illusion Project
+ * Copyright (C) 2015 Androis Open Source Illusion Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,51 +16,81 @@
 
 package com.android.settings.aosip.buttons;
 
-import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
-import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.Utils;
 
 public class VolumeCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+    private static final String TAG = "Buttons";
 
-    private static final String RAS_VOLUME_KEYS_CURSOR_CONTROL = "ras_volume_keys_cursor_control";
+    private static final String KEY_VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
-    private ListPreference volumeKeysCursorControlListPref;
+    private ListPreference mVolumeKeyCursorControl;
+    private SwitchPreference mVolumeRockerWake;
 
     @Override
-    protected int getMetricsCategory() {
-        return MetricsLogger.OWLSNEST;
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.aosip_volume);
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        int cursorControlAction = Settings.System.getInt(resolver,
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
+            mVolumeKeyCursorControl = initActionList(KEY_VOLUME_KEY_CURSOR_CONTROL,
+                    cursorControlAction);
+
+        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
+        mVolumeRockerWake.setOnPreferenceChangeListener(this);
+        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_WAKE, 0);
+        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private ListPreference initActionList(String key, int value) {
+        ListPreference list = (ListPreference) getPreferenceScreen().findPreference(key);
+        list.setValue(Integer.toString(value));
+        list.setSummary(list.getEntry());
+        list.setOnPreferenceChangeListener(this);
+        return list;
+    }
 
-        addPreferencesFromResource(R.xml.aosip_volume);
+    private void handleActionListChange(ListPreference pref, Object newValue, String setting) {
+        String value = (String) newValue;
+        int index = pref.findIndexOfValue(value);
 
-        // volume keys cursor control
-        volumeKeysCursorControlListPref = (ListPreference) findPreference(
-                RAS_VOLUME_KEYS_CURSOR_CONTROL);
-        if (volumeKeysCursorControlListPref != null) {
-            volumeKeysCursorControlListPref.setOnPreferenceChangeListener(this);
-            volumeKeysCursorControlListPref
-                    .setValue(Integer.toString(Settings.System.getInt(getContentResolver(),
-                            RAS_VOLUME_KEYS_CURSOR_CONTROL,
-                            0)));
-            volumeKeysCursorControlListPref.setSummary(volumeKeysCursorControlListPref.getEntry());
-        }
+        pref.setSummary(pref.getEntries()[index]);
+        Settings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
     }
 
     @Override
@@ -68,19 +98,26 @@ public class VolumeCategory extends SettingsPreferenceFragment implements
         super.onResume();
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        // volume keys cursor control
-        if (preference == volumeKeysCursorControlListPref) {
-            int volumeKeyCursorControlValue = Integer.parseInt((String) newValue);
-            Settings.System.putInt(getContentResolver(), RAS_VOLUME_KEYS_CURSOR_CONTROL,
-                    volumeKeyCursorControlValue);
-            int volumeKeyCursorControlIndex = volumeKeysCursorControlListPref
-                    .findIndexOfValue((String) newValue);
-            volumeKeysCursorControlListPref.setSummary(
-                    volumeKeysCursorControlListPref.getEntries()[volumeKeyCursorControlIndex]);
+        ContentResolver resolver = getActivity().getContentResolver();
+        final String key = preference.getKey();
+        if (preference == mVolumeKeyCursorControl) {
+            handleActionListChange(mVolumeKeyCursorControl, newValue,
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL);
             return true;
+        } else if (preference == mVolumeRockerWake) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
+                    value ? 1 : 0);
+         return true;
         }
         return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.OWLSNEST;
     }
 }
 

Validus-Shamu/packages_apps_Settings

src/com/android/settings/validus/buttons/VolumeCategory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Android Open Source Illusion Project
+ * Copyright (C) 2015 Androis Open Source Illusion Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,51 +16,81 @@
 
 package com.android.settings.validus.buttons;
 
-import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceScreen;
-import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.Utils;
 
 public class VolumeCategory extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+    private static final String TAG = "Buttons";
 
-    private static final String RAS_VOLUME_KEYS_CURSOR_CONTROL = "ras_volume_keys_cursor_control";
+    private static final String KEY_VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
-    private ListPreference volumeKeysCursorControlListPref;
+    private ListPreference mVolumeKeyCursorControl;
+    private SwitchPreference mVolumeRockerWake;
 
     @Override
-    protected int getMetricsCategory() {
-        return MetricsLogger.THE_WOLF;
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.validus_volume);
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        int cursorControlAction = Settings.System.getInt(resolver,
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
+            mVolumeKeyCursorControl = initActionList(KEY_VOLUME_KEY_CURSOR_CONTROL,
+                    cursorControlAction);
+
+        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
+        mVolumeRockerWake.setOnPreferenceChangeListener(this);
+        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_WAKE, 0);
+        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private ListPreference initActionList(String key, int value) {
+        ListPreference list = (ListPreference) getPreferenceScreen().findPreference(key);
+        list.setValue(Integer.toString(value));
+        list.setSummary(list.getEntry());
+        list.setOnPreferenceChangeListener(this);
+        return list;
+    }
 
-        addPreferencesFromResource(R.xml.validus_volume);
+    private void handleActionListChange(ListPreference pref, Object newValue, String setting) {
+        String value = (String) newValue;
+        int index = pref.findIndexOfValue(value);
 
-        // volume keys cursor control
-        volumeKeysCursorControlListPref = (ListPreference) findPreference(
-                RAS_VOLUME_KEYS_CURSOR_CONTROL);
-        if (volumeKeysCursorControlListPref != null) {
-            volumeKeysCursorControlListPref.setOnPreferenceChangeListener(this);
-            volumeKeysCursorControlListPref
-                    .setValue(Integer.toString(Settings.System.getInt(getContentResolver(),
-                            RAS_VOLUME_KEYS_CURSOR_CONTROL,
-                            0)));
-            volumeKeysCursorControlListPref.setSummary(volumeKeysCursorControlListPref.getEntry());
-        }
+        pref.setSummary(pref.getEntries()[index]);
+        Settings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
     }
 
     @Override
@@ -68,19 +98,25 @@ public class VolumeCategory extends SettingsPreferenceFragment implements
         super.onResume();
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        // volume keys cursor control
-        if (preference == volumeKeysCursorControlListPref) {
-            int volumeKeyCursorControlValue = Integer.parseInt((String) newValue);
-            Settings.System.putInt(getContentResolver(), RAS_VOLUME_KEYS_CURSOR_CONTROL,
-                    volumeKeyCursorControlValue);
-            int volumeKeyCursorControlIndex = volumeKeysCursorControlListPref
-                    .findIndexOfValue((String) newValue);
-            volumeKeysCursorControlListPref.setSummary(
-                    volumeKeysCursorControlListPref.getEntries()[volumeKeyCursorControlIndex]);
+        ContentResolver resolver = getActivity().getContentResolver();
+        final String key = preference.getKey();
+        if (preference == mVolumeKeyCursorControl) {
+            handleActionListChange(mVolumeKeyCursorControl, newValue,
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL);
             return true;
+        } else if (preference == mVolumeRockerWake) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
+                    value ? 1 : 0);
+         return true;
         }
         return false;
     }
-}
 
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.THE_WOLF;
+    }
+}