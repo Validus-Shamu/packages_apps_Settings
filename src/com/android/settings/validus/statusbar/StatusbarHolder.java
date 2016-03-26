xlxfoxxlx/platform_packages_apps_Settings-2

AOSiP Logo on Status Bar (2/2)
PS2: Code fixup
PS3: fix layout

Change-Id: Ia6309c8039cc3484b0220527d741cf26c6c4fa9e

Conflicts:
	res/xml/statusbar_layout.xml
	src/com/lordclockan/aicpextras/StatusBarFragment.java

src/com/android/settings/aosip/statusbar/StatusbarHolder.java
@@ -55,6 +55,7 @@ import com.android.settings.aosip.statusbar.ClockCategory;
 import com.android.settings.aosip.statusbar.CarrierlabelCategory;
 import com.android.settings.aosip.statusbar.BatteryCategory;
 import com.android.settings.aosip.statusbar.IconsCategory;
+import com.android.settings.aosip.statusbar.AosipLogo;
 import com.android.settings.aosip.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
@@ -114,6 +115,7 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
             frags[2] = new IconsCategory();
             frags[3] = new CarrierlabelCategory();
             frags[4] = new TrafficCategory();
+            frags[5] = new AosipLogo();
         }
 
         @Override
@@ -139,7 +141,8 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
                     getString(R.string.battery_category),
                     getString(R.string.icon_category),
                     getString(R.string.carrier_category),
-                    getString(R.string.traffic_category)};
+                    getString(R.string.traffic_category),
+                    getString(R.string.aosip_logo)};
         return titleString;
     }
 }

Validus-Shamu/packages_apps_Settings

src/com/android/settings/validus/statusbar/StatusbarHolder.java
@@ -55,6 +55,7 @@ import com.android.settings.validus.statusbar.ClockCategory;
 import com.android.settings.validus.statusbar.CarrierlabelCategory;
 import com.android.settings.validus.statusbar.BatteryCategory;
 import com.android.settings.validus.statusbar.IconsCategory;
+import com.android.settings.validus.statusbar.ValidusLogo;
 import com.android.settings.validus.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
@@ -114,6 +115,7 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
             frags[2] = new IconsCategory();
             frags[3] = new CarrierlabelCategory();
             frags[4] = new TrafficCategory();
+            frags[5] = new ValidusLogo();
         }
 
         @Override
@@ -139,7 +141,8 @@ public class StatusbarHolder extends SettingsPreferenceFragment {
                     getString(R.string.battery_category),
                     getString(R.string.icon_category),
                     getString(R.string.carrier_category),
-                    getString(R.string.traffic_category)};
+                    getString(R.string.traffic_category),
+                    getString(R.string.validus_logo)};
         return titleString;
     }
 }